5377918c345150126339d2bc068820e0fe031ea2
apelak <92027563+apelak@users.noreply.github.com>
2022-10-14 11:41:18 +0200

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,45 @@
+#Zadanie 1
+
+# def zad1(imie,nazwisko):
+#     return(imie + '.' + nazwisko)
+# print(zad1('J','Kowalski'))
+
+#Zadanie 2
+
+# def zad2(imie,nazwisko):
+#     return(imie[0] + '.' + nazwisko)
+# print(zad2('Jan','Kowalski'))
+
+#Zadanie 3
+# def zad3(dwiePierwsze,dwieOstatnie,wiekUzytkownika):
+#
+#     if wiekUzytkownika > dwieOstatnie:
+#         return(str(dwiePierwsze-1)+str(dwieOstatnie+100-wiekUzytkownika))
+#     else:
+#         return(str(dwiePierwsze)+'0'+str(dwieOstatnie-wiekUzytkownika))
+#
+# print(zad3(20,22,23))
+
+#Zadanie 4
+
+# def zad4(imie,nazwisko,zad2):
+#     return(zad2(imie,nazwisko))
+# print(zad4('Jan','Kowalski',zad2))
+
+
+#Zadanie 5
+#
+# def zad5(a,b):
+#     if a >0 and b > 0 and b !=0:
+#         return(float(a/b))
+# print(zad5(5,3))
+
+#Zadanie 6
+# x=0
+# while x < 100:
+#     x += int(input())
+#     print(x)
+
+#Zadanie 7
+
+
